2deb9f7f4eb6bf954a3bae9a5f0ad4dacebfff6c
JuS <sagurna@s3i.de>
2016-02-12 12:45:55 +0100

    Erste Vorbereitung für ffm-test mit Gluon v2016.1

--- site.mk
+++ site.mk
@@ -1,7 +1,7 @@
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-14 \
 	gluon-alfred \
-	gluon-announced \
+	gluon-respondd \
 	gluon-autoupdater \
 	gluon-setup-mode \
 	gluon-config-mode-core \
@@ -16,17 +16,24 @@ GLUON_SITE_PACKAGES := \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
 	gluon-luci-private-wifi \
-	gluon-luci-mesh-vpn-fastd \
 	gluon-luci-wifi-config \
+	gluon-luci-node-role \
+	ffffm-luci-switchconfig \
+	ffffm-luci-set-mtu \
+    ffffm-fastd-auto-mtu \
+    ffffm-keep-radio-channel \
 	gluon-next-node \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
 	gluon-status-page \
 	iwinfo \
 	iptables \
-	gluon-luci-switchconfig \
-	ffffm-ebtables-net-rules \
-	haveged 
+	haveged \
+    iputils-ping \
+
+#	ffffm-ebtables-net-rules \     # Das Package macht Probleme beim Aufruf der Statusseite andere Router 
+#   gluon-luci-mesh-vpn-fastd \    # Hier sollten wir mal drüber sprechen.
+
 #	gluon-ffmcollectdconfig \
 #	collectd5 \
 #	collectd-mod-ping \
@@ -38,13 +45,23 @@ GLUON_SITE_PACKAGES := \
 #	collectd-mod-memory \
 #	gluon-ffmautokey \
 
-DEFAULT_GLUON_RELEASE := 2015.1.2-$(shell date '+%Y%m%d')
+#####################################################################################################################
+
+# This is the dev branch
+DEFAULT_GLUON_RELEASE := 2016.1-test
+
+# For homebrew development add e.g. date and time 
+# (don't use the ':' char. It will break the build)
+# DEFAULT_GLUON_RELEASE := $(DEFAULT_GLUON_RELEASE)-$(shell date '+%m.%d-%H%M')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
+# Development branch information
+GLUON_BRANCH ?= test
+
 # Default priority for updates.
-GLUON_PRIORITY ?= 0
+# GLUON_PRIORITY ?= 0
 
 # Languages to include
 GLUON_LANGS ?= en de
