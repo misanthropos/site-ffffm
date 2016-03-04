54ceab2d8b8c2e230fe68dff2823e42e4508f1fc
JuS <sagurna@s3i.de>
2016-03-04 17:38:40 +0100

    test: site.mk schön gemacht

--- site.mk
+++ site.mk
@@ -1,54 +1,44 @@
 GLUON_SITE_PACKAGES := \
-	gluon-mesh-batman-adv-14 \
-	gluon-alfred \
-	gluon-respondd \
-	gluon-autoupdater \
-	gluon-setup-mode \
-	gluon-config-mode-core \
-	gluon-config-mode-autoupdater \
-	gluon-config-mode-hostname \
-	gluon-config-mode-mesh-vpn \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-contact-info \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-filter-ra-dhcp \
-	gluon-luci-admin \
-	gluon-luci-autoupdater \
-	gluon-luci-portconfig \
-	gluon-luci-private-wifi \
-	gluon-luci-wifi-config \
-	gluon-luci-node-role \
-	ffffm-luci-switchconfig \
-	ffffm-luci-set-mtu \
+    gluon-mesh-batman-adv-14 \
+    gluon-alfred \
+    gluon-respondd \
+    gluon-autoupdater \
+    gluon-setup-mode \
+    gluon-config-mode-core \
+    gluon-config-mode-autoupdater \
+    gluon-config-mode-hostname \
+    gluon-config-mode-mesh-vpn \
+    gluon-config-mode-geo-location \
+    gluon-config-mode-contact-info \
+    gluon-ebtables-filter-multicast \
+    gluon-ebtables-filter-ra-dhcp \
+    gluon-luci-admin \
+    gluon-luci-autoupdater \
+    gluon-luci-portconfig \
+    gluon-luci-private-wifi \
+    gluon-luci-wifi-config \
+    gluon-luci-node-role \
+    ffffm-luci-switchconfig \
+    ffffm-luci-set-mtu \
     ffffm-fastd-auto-mtu \
     ffffm-keep-radio-channel \
-	gluon-next-node \
-	gluon-mesh-vpn-fastd \
-	gluon-radvd \
-	gluon-status-page \
-	iwinfo \
-	iptables \
-	haveged \
+    gluon-next-node \
+    gluon-mesh-vpn-fastd \
+    gluon-radvd \
+    gluon-status-page \
+    iwinfo \
+    iptables \
+    haveged \
     iputils-ping \
+    gluon-luci-mesh-vpn-fastd \    # Hier sollten wir noch mal drüber sprechen.
 
-#	ffffm-ebtables-net-rules \     # Das Package macht Probleme beim Aufruf der Statusseite andere Router 
-#   gluon-luci-mesh-vpn-fastd \    # Hier sollten wir mal drüber sprechen.
-
-#	gluon-ffmcollectdconfig \
-#	collectd5 \
-#	collectd-mod-ping \
-#	collectd-mod-interface \
-#	collectd-mod-cpu \
-#	collectd-mod-network \
-#	collectd-mod-iwinfo \
-#	collectd-mod-load \
-#	collectd-mod-memory \
-#	gluon-ffmautokey \
 
 #####################################################################################################################
 
 # This is the test branch
-DEFAULT_GLUON_RELEASE := 2016.1-test
+
+# Gluon Base Release
+# DEFAULT_GLUON_RELEASE := 2016.1.1
 
 # For homebrew development add e.g. date and time 
 # (don't use the ':' char. It will break the build)
