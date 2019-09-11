26e8c0af3ec42c86439ae912d2e1c40ac4f5db12
JuS <sagurna@s3i.de>
2019-09-11 15:02:04 +0200

    Anpassung der "site.mk" in Bezug auf Homebrew-Firmware-Bau

--- site.mk
+++ site.mk
@@ -22,7 +22,6 @@ GLUON_SITE_PACKAGES := \
 	haveged \
 	ffffm-banner_legacy \
 	ffffm-keep-radio-channel \
-	ffffm-autoupdater-use-site-conf-branch \
 	ffffm-button-bind \
 	ffffm-ath9k-broken-wifi-workaround \
 	gluon-ssid-changer \
@@ -32,32 +31,68 @@ GLUON_SITE_PACKAGES := \
 
 include $(GLUON_SITEDIR)/specific_site.mk
 
+# Enable multidomain support
+GLUON_MULTIDOMAIN := 1
 
-#####################################################################################################################
 
-# This is the Test branch
+# This is the 'Test' site branch.
 
-# Gluon Base Release
-DEFAULT_GLUON_RELEASE := vHomebrew-test
+########################################################################
+# BEGIN: "Generic homebrew build preparation"
+#
+# To build an own Frankfurt firmware please see:
+# https://github.com/freifunk-ffm/site-ffffm#eine-eigene-frankfurter-firmware-bauen
+########################################################################
+
+# Homebrew Gluon firmware release name
+HOMEBREW_GLUON_RELEASE := vHomebrew-test
 
 # For homebrew development add e.g. date and time
 # (Note: Don't use the ':' char. It will break the build)
-DEFAULT_GLUON_RELEASE := $(DEFAULT_GLUON_RELEASE)-$(shell date '+%Y.%m.%d')
+HOMEBREW_GLUON_RELEASE := $(HOMEBREW_GLUON_RELEASE)-$(shell date '+%m%d')
 
-# Allow overriding the release number from the command line
-GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+########################################################################
+# END: "Generic homebrew build preparation"
+#
+# Admin/Support hint:
+# With the few lines above you can identify exactly which branch of our
+# site was used to create the homebrew firmware.
+########################################################################
 
-# Development branch information
+###
+
+########################################################################
+# Normally the important parameters are specified by the superimposed
+# build process (e.g. CI and/or CD).
+# If this is not the case the following '?=' assignments apply.
+########################################################################
+
+# Gluon release information.
+# Allow overriding from the command line.
+GLUON_RELEASE ?= $(HOMEBREW_GLUON_RELEASE)
+
+# Autoupdater default branch.
+# Allow overriding from the command line.
 GLUON_BRANCH ?= test
 
 # Default priority for updates.
+# Allow overriding from the command line.
 GLUON_PRIORITY ?= 0
 
-# Languages to include
+# Languages to include.
+# Allow overriding from the command line.
 GLUON_LANGS ?= de
 
-# region information for regulatory compliance
+# Region information for regulatory compliance.
+# Allow overriding from the command line.
 GLUON_REGION ?= eu
 
-# Prefer ath10k firmware with given mesh support (ibss or 11s)
+# Prefer ath10k firmware with given mesh support (ibss or 11s).
+# Allow overriding from the command line.
 GLUON_WLAN_MESH ?= 11s
+
+# Build of image types.
+# Allow overriding from the command line.
+GLUON_DEPRECATED ?= full
+
+###
